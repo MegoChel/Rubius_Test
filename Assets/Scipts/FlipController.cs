1ceff990def9f102d9c342993f22732a498b04bc
MegoChel <meg4el.ivan@gmail.com>
2021-11-07 00:00:50 +0700

    2 out of 3

--- Assets/Scipts/FlipController.cs
+++ Assets/Scipts/FlipController.cs
@@ -2,35 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class FlipController : MonoBehaviour
 {
+    public float flipTime = 0.5f;
     public GameObject[] cards;
+    public Dropdown dropdown;
+    //private ArrayList readyCards;
+    //private ArrayList notReadyCards;
+    //private bool onReady = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //notReadyCards = new ArrayList(cards);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if(notReadyCards.Count != 0)
+        //    foreach (GameObject item in notReadyCards)
+        //    {
+        //        if (item.tag == "ReadyToFlip")
+        //            {
+        //                readyCards.Add(item);
+        //                notReadyCards.Remove(item);
+        //            }
+        //    }
+
+    }
+
+    private IEnumerator FlipAllAtOnce()
+    {
+        gameObject.GetComponent<Button>().interactable = false;
+        foreach (var item in cards)
+        {
+            var seq = DOTween.Sequence();
+            item.transform.DORotate((item.transform.eulerAngles + new Vector3(0, 180, 0)), flipTime, RotateMode.Fast);
+            seq.Append(item.transform.DOScale(new Vector3(1.25f, 1.25f, 1.25f), flipTime / 2));
+            seq.Append(item.transform.DOScale(new Vector3(1.0f, 1.0f, 1.0f), flipTime / 2));
+            
+        }
+        yield return new WaitForSeconds(flipTime);
+        gameObject.GetComponent<Button>().interactable = true;
+    }
+    private IEnumerator FlipOneByOne()
+    {
+        gameObject.GetComponent<Button>().interactable = false;
+        var seq = DOTween.Sequence();
+        var seqf = DOTween.Sequence();
+        foreach (var item in cards)
+        {
+            seq.Append(item.transform.DORotate((item.transform.eulerAngles + new Vector3(0, 180, 0)), flipTime, RotateMode.Fast));
+            seqf.Append(item.transform.DOScale(new Vector3(1.25f, 1.25f, 1.25f), flipTime / 2));
+            seqf.Append(item.transform.DOScale(new Vector3(1.0f, 1.0f, 1.0f), flipTime / 2));
+        }
+        yield return seq.WaitForCompletion();
+        gameObject.GetComponent<Button>().interactable = true;
+    }
+    private void FlipOnReady()
+    {
         
     }
 
-    public void FlipAllAtOnce()
+    public void ButtonClick()
     {
-        foreach (var item in cards)
+        switch (dropdown.value)
         {
-            item.GetComponent<Flip>().FlipCard();
+            case 0:
+                StartCoroutine(FlipAllAtOnce());
+                break;
+            case 1:
+                StartCoroutine(FlipOneByOne());
+                break;
+            case 2:
+                FlipOnReady();
+                break;
         }
-    }
-    public void FlipOneByOne()
-    {
-        var seq = DOTween.Sequence();
-        foreach (var item in cards)
-        {
-            //seq.Append(item.GetComponent<Flip>().FlipCard());
-        }
+         
     }
 }
--- Assets/Scipts/PicLoad.cs
+++ Assets/Scipts/PicLoad.cs
@@ -11,7 +11,7 @@ public class PicLoad : MonoBehaviour
         string url = "https://picsum.photos/seed/" + Random.value.ToString() + "/153/212";
         StartCoroutine(DownloadImage(url));
     }
-
+    public bool isLoaded = false;
     IEnumerator DownloadImage(string MediaUrl)
     {
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
@@ -22,12 +22,14 @@ public class PicLoad : MonoBehaviour
         {
             Texture2D text2d = ((DownloadHandlerTexture)request.downloadHandler).texture;
             gameObject.GetComponent<SpriteRenderer>().sprite = Sprite.Create(text2d, new Rect(0.0f, 0.0f, text2d.width, text2d.height), new Vector2(0.5f, 0.5f));
+            isLoaded = true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isLoaded)
+            transform.root.gameObject.tag = "ReadyToFlip";
     }
 }
